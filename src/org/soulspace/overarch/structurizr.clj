09d14e5714433c811e3a51c4784ff8a1af99b6fd
Ludger Solbach <soulman@soulspace.org>
2023-05-16 11:10:31 +0200

    enhanced structurizr export (wip)

--- src/org/soulspace/overarch/structurizr.clj
+++ src/org/soulspace/overarch/structurizr.clj
@@ -4,13 +4,50 @@
             [org.soulspace.overarch.core :as core]
             [org.soulspace.overarch.diagram :as dia]))
 
+(def element-type->structurizr
+  "Maps the element to a structurizr type."
+  {:person "person"
+   :system "softwaresystem"
+   :container "container"
+   :component "component"
+   :node "deploymentNode"})
+
+(def diagram-type->structurizr
+  "Maps the diagram type"
+  {:system-landscape-diagram "systemlandscape"
+   :context-diagram "systemcontext"
+   :container-diagram "container"
+   :component-diagram "component"
+   :deployment-diagram "deployment"
+   :dynamic-diagram "dynamic"})
+
+(defn alias-name
+  "Returns the alias name for the element."
+  [id]
+  (name id))
+
+
+(defn render-relation
+  "Renders a structurizr relation."
+  ([e]
+   [(str (alias-name (:from e)) " -> " (alias-name (:to e))
+         " \"" (:name e) "\""
+         (when (:tech e) (str " \"" (:tech e) "\"")))]))
 
 (defn render-element
-  "Renders a structurizr views model element."
-  [e]
-  [(str (dia/render-indent 4) "{")
-   (str (dia/render-indent 4) "}")]
-  )
+  "Renders a structurizr model element."
+  ([e]
+   (render-element 4 e))
+  ([indent e]
+   (if (:ct e)
+     [(str (dia/render-indent indent) (alias-name e) " = "
+           (element-type->structurizr (:el e)) " "
+           (:name e) "{")
+      (map (partial render-element (+ indent 2)) (:ct e))
+      (str (dia/render-indent indent) "}")]
+     [(str (dia/render-indent indent) (alias-name e) " = "
+           (element-type->structurizr (:el e)) " "
+           (:name e))])))
 
 (defn render-model
   "Renders the structurizr model."
