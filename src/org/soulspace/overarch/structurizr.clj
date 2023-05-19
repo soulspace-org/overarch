12c5b786bbfc490646c8fd753f64085789cdc0a2
Ludger Solbach <soulman@soulspace.org>
2023-05-19 13:27:27 +0200

    enhanced structurizr export (wip)

--- src/org/soulspace/overarch/structurizr.clj
+++ src/org/soulspace/overarch/structurizr.clj
@@ -22,70 +22,84 @@
    :deployment-diagram "deployment"
    :dynamic-diagram "dynamic"})
 
+(def element-hierarchy
+  "Hierarchy for rendering elements."
+  (-> (make-hierarchy)
+      (derive :person :model-element)
+      (derive :system :model-element)
+      (derive :container :model-element)
+      (derive :component :model-element)
+      (derive :node :model-element)
+      (derive :enterprise-boundary :model-element)
+      ))
+
 (defn alias-name
   "Returns the alias name for the element."
   [id]
   (sstr/hyphen-to-camel-case (name id)))
 
-
-(defn render-relation
-  "Renders a structurizr relation."
-  ([e]
-   [(str (alias-name (:from e)) " -> " (alias-name (:to e))
-         " \"" (:name e) "\""
-         (when (:tech e) (str " \"" (:tech e) "\"")))]))
-
-(defn render-element
+(defmulti render-element
   "Renders a structurizr model element."
-  ([e]
-   (render-element 4 e))
-  ([indent e]
-   (if (:ct e)
-     [(str (dia/render-indent indent) (alias-name e) " = "
-           (element-type->structurizr (:el e)) " "
-           (:name e) "{")
-      (map (partial render-element (+ indent 2)) (:ct e))
-      (str (dia/render-indent indent) "}")]
-     [(str (dia/render-indent indent) (alias-name e) " = "
-           (element-type->structurizr (:el e)) " "
-           (:name e))])))
+  (fn [indent e] (:el e))
+  :hierarchy #'element-hierarchy)
+
+(defmethod render-element :rel
+  [indent e]
+  [(str (alias-name (:from e)) " -> " (alias-name (:to e))
+        " \"" (:name e) "\""
+        (when (:tech e) (str " \"" (:tech e) "\"")))])
+
+(defmethod render-element :model-element
+  [indent e]
+  (if (:ct e)
+    (let [children (map core/resolve-ref (:ct e))]
+      [(str (dia/render-indent indent) (alias-name (:id e)) " = "
+            (element-type->structurizr (:el e))
+            " \"" (dia/element-name e) "\" {")
+       (map (partial render-element (+ indent 2)) children)
+       (str (dia/render-indent indent) "}")])
+    [(str (dia/render-indent indent) (alias-name (:id e)) " = "
+          (element-type->structurizr (:el e))
+          " \"" (dia/element-name e) "\"")]))
 
 (defn render-model
   "Renders the structurizr model."
   [elements]
-  [(str (dia/render-indent 2) "model {")
-   (map render-element elements)
-   (str (dia/render-indent 2) "}")]
+  (flatten [(str (dia/render-indent 2) "model {")
+            (map (partial render-element 4)  elements)
+            (str (dia/render-indent 2) "}")])
   )
 
+; TODO render rels only
 (defn render-view-element
   "Renders an element of a view."
-  [e]
-  [(str (:id e))]
+  [indent e]
+  [(str (dia/render-indent indent) (:id e))]
   )
 
 (defn render-view
   "Renders a structurizr view."
   [view]
-  [(str (dia/render-indent 4)
-        (diagram-type->structurizr (:el view))
-        "\"" (sstr/first-upper-case
-              (sstr/hyphen-to-camel-case
-               (diagram-type->structurizr (:el view)))) "\" {\n")
-   (if (:ct view)
-     (map render-view-element (:ct view))
-     (str (dia/render-indent 6) "include *\n"))
-   (when (:title view) (dia/render-indent 6) "description \"" (:title view) "\"\n")
-   (str (dia/render-indent 4) "}")]
-  )
+  (flatten [(str (dia/render-indent 4)
+                 (diagram-type->structurizr (:el view))
+                 " \"" (sstr/first-upper-case
+                        (sstr/hyphen-to-camel-case
+                         (diagram-type->structurizr (:el view)))) "\" {\n")
+            (if (:ct view)
+              (let [children (dia/elements-to-render view)]
+                (map (partial render-view-element 6) children))
+              (str (dia/render-indent 6) "include *\n"))
+            (when (:title view)
+              (str (dia/render-indent 6) "description \"" (:title view) "\"\n"))
+            (str (dia/render-indent 4) "}")]))
 
 (defn render-views
   "Renders the structurizr views."
   [views]
-  [(str (dia/render-indent 2) "views {")
-   (map render-view views)
-   (str (dia/render-indent 4) "theme default")
-   (str (dia/render-indent 2) "}")]
+  (flatten [(str (dia/render-indent 2) "views {")
+            (map render-view views)
+            (str (dia/render-indent 4) "theme default")
+            (str (dia/render-indent 2) "}")])
   )
 
 (defn render-workspace
@@ -102,5 +116,5 @@
     "}"]))
 
 (comment
-  (render-workspace)
+  (println (str/join "\n" (flatten (render-workspace))))
   )
