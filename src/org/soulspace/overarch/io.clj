5c8c040a8d98a80d320b625a3338f30a06905d22
Ludger Solbach <soulman@soulspace.org>
2023-06-08 12:49:25 +0200

    added io namespace to collect the generic I/O fns

---
+++ src/org/soulspace/overarch/io.clj
@@ -0,0 +1,27 @@
+(ns org.soulspace.overarch.io
+  "Contains I/O related functions."
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [charred.api :as json]))
+
+(defn load-edn-from-resource
+  "Returns the data from the resource named `filename`."
+  [filename]
+  (-> filename
+      (io/resource)
+      (slurp)
+      (edn/read-string)))
+
+(defn load-edn
+  "Returns the data from the file named `filename`."
+  [filename]
+  (-> filename
+      (slurp)
+      (edn/read-string)))
+
+(defn write-json
+  "Writes the data as JSON to `filename`."
+  [filename data]
+  (with-open [wrt (io/writer filename)]
+    (json/write-json wrt data)))
+
