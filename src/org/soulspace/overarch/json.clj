50311f36bcddd34634f6320e61a1b3fb68bb068a
Ludger Solbach <soulman@soulspace.org>
2023-06-09 08:57:41 +0200

    use functions from io namespace

--- src/org/soulspace/overarch/json.clj
+++ src/org/soulspace/overarch/json.clj
@@ -3,26 +3,19 @@
             [clojure.java.io :as io]
             [charred.api :as json]
             [org.soulspace.clj.java.file :as file]
-            [org.soulspace.overarch.export :as exp]))
-
-(defn write-json
-  "Writes the data as JSON to out-file."
-  [out-file data]
-  (with-open [wrt (io/writer out-file)]
-    (json/write-json wrt data)))
+            [org.soulspace.overarch.export :as exp]
+            [org.soulspace.overarch.io :as oio]))
 
 (defn export-json
-  "Exports the data files in the given directory to JSON."
+  "Exports the data files in the model directory, as specified by `options`, to JSON."
   [options]
   (doseq [file (file/all-files-by-extension "edn" (:model-dir options))]
     (let [out-dir (str (:export-dir options)"/"
                        (file/parent-path file) "/")
           out-file (str out-dir (file/base-name file) ".json")]
-      (file/create-dir out-dir)
       (->> file
-           (slurp)
-           (edn/read-string)
-           (write-json out-file)))))
+           (oio/load-edn)
+           (oio/write-json out-file)))))
 
 (defmethod exp/export :json
   [options]
