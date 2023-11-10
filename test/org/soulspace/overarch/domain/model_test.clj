1b2847b4146be279c6068fb36cffe6fc0481e166
Ludger Solbach <soulman@soulspace.org>
2023-11-10 09:27:32 +0100

    enhanced tests

--- test/org/soulspace/overarch/domain/model_test.clj
+++ test/org/soulspace/overarch/domain/model_test.clj
@@ -23,44 +23,85 @@
      :to :test/system1
      :name "uses"}})
 
-(deftest model-predicates-test
+(def concept-model
+  #{{:el :concept
+     :id :test/concept1
+     :name "Concept 1"}
+    {:el :concept
+     :id :test/concept2
+     :name "Concept 2"}
+    {:el :concept
+     :id :test/concept3
+     :name "Concept 3"}
+    {:el :rel
+     :id :test/concept1-has-a-concept2
+     :from :test/concept1
+     :to :test/concept2
+     :name "has a"}
+    {:el :rel
+     :id :test/concept1-is-a-concept3
+     :from :test/concept1
+     :to :test/concept3
+     :name "is a"}
+    })
+
+(deftest element?-test
   (testing "element?"
     (are [x y] (= x (fns/truthy? (element? y)))
       true {:el :person}
       false {}
-      false {:type :person}))
+      false {:type :person})))
 
+(deftest identifiable?-test
   (testing "identifiable?"
     (are [x y] (= x (fns/truthy? (identifiable? y)))
       true {:id :abc}
       true {:id :a/abc}
       false {}
-      false {:type :person}))
+      false {:type :person})))
 
+(deftest named?-test
   (testing "named?"
     (are [x y] (= x (fns/truthy? (named? y)))
       true {:name "abc"}
       false {}
-      false {:type :person}))
+      false {:type :person})))
 
+(deftest relational?-test
   (testing "relational?"
     (are [x y] (= x (fns/truthy? (relational? y)))
       true {:from :abc :to :bcd}
       true {:from :a/abc :to :a/bcd}
       false {}
-      false {:type :person}))
+      false {:type :person})))
 
+(deftest external?-test
   (testing "external?"
     (are [x y] (= x (fns/truthy? (external? y)))
       true {:external true}
       false {:external false}
       false {}
-      false {:type :person}))
+      false {:type :person})))
 
+(deftest reference?-test
   (testing "reference?"
     (are [x y] (= x (fns/truthy? (reference? y)))
       true {:ref :abc}
       true {:ref :a/abc}
       false {}
       false {:type :person})))
+
+(deftest build-id->elements-test
+      (testing "build-id->elements"
+        (are [x y] (= x y)
+          5 (count (build-id->elements c4-model))
+          5 (count (build-id->elements concept-model))
+          )))
+
+(deftest build-id->parent-test
+  (testing "build-id->parent"
+    (are [x y] (= x y)
+      2 (count (build-id->parent c4-model))
+      0 (count (build-id->parent concept-model)))))
+
 
