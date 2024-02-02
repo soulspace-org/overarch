52c14983530ac1cf77a28731671ca32e8b3b44a9
Ludger Solbach <soulman@soulspace.org>
2024-02-02 09:16:47 +0100

    added tests

--- test/org/soulspace/overarch/util/functions_test.clj
+++ test/org/soulspace/overarch/util/functions_test.clj
@@ -1,3 +1,22 @@
 (ns org.soulspace.overarch.util.functions-test
   (:require [clojure.test :refer :all]
             [org.soulspace.overarch.util.functions :refer :all]))
+
+
+(deftest compile-test
+  (testing "Compilation"
+    (is (= 1 1))))
+
+(deftest truthy?-test
+  (testing "truthy? false"
+    (are [x y] (= x (truthy? y))
+      false false
+      false nil))
+    (testing "truthy? true"
+    (are [x y] (= x (truthy? y))
+      true true
+      true "true"
+      true "false"
+      true 0
+      true 1
+      true '())))
--- test/org/soulspace/overarch/util/io_test.clj
+++ test/org/soulspace/overarch/util/io_test.clj
@@ -1,3 +1,9 @@
 (ns org.soulspace.overarch.util.io-test
   (:require [clojure.test :refer :all]
             [org.soulspace.overarch.util.io :refer :all]))
+
+
+(deftest compile-test
+  (testing "Compilation"
+    (is (= 1 1))))
+
