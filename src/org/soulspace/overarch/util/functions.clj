6c882e0f8b8bba4011cd094ad70703103c6bbc0d
Ludger Solbach <soulman@soulspace.org>
2024-05-28 22:37:25 +0200

    added generic functions

--- src/org/soulspace/overarch/util/functions.clj
+++ src/org/soulspace/overarch/util/functions.clj
@@ -21,6 +21,26 @@
         (map str/trim)
         (into []))))
 
+(defn wrap-str
+  "Wraps the argument `s` in double quotes."
+  [s]
+  (str "\"" s "\""))
+
+(defn binding-vector
+  "Creates a binding vector for the entries of the data map `m`."
+  [m]
+  (let [keysyms (->> (keys m)
+                     (map name)
+                     (map symbol))
+        values (vals m)]
+    (into [] (interleave keysyms values))))
+
+(comment
+  (binding-vector {:e {:el :system
+                       :id :foo/foo-bar}})
+  )
+
+
 ;;
 ;; Tapping data
 ;;
