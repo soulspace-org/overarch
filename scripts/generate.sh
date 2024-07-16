2172a9fee5f218350cc716218f0aedb976f133d9
Ludger Solbach <soulman@soulspace.org>
2024-07-16 08:50:45 +0200

    added todos to script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
 
+# read model and generate output with overarch
 java -jar overarch.jar -r all -g ./gencfg.edn
 
+# generate diagrams with PlantUML
+
+# generate diagrams with GraphViz dot
+
+# copy results?
