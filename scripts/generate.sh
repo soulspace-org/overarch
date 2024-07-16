7aa96d076a4bde3d0ac68e5b38cb673dca9f8f9f
Solbach, Ludger (FDP4_EXTERN) <ludger.solbach@msg.group>
2024-07-16 14:35:05 +0200

    updated script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
 # read model and generate output with overarch
-java -jar overarch.jar -r all -g ./gencfg.edn
+java -jar overarch.jar --no-render-format-subdirs -R generated -r all -g ./gencfg.edn
 
 # generate diagrams with PlantUML
+find generated -type f -name "*.puml" | while read file; do
+  target_dir=$(dirname "$file")
+  mkdir -p "$target_dir"
+  cat "$file" | java -Xmx1024m -jar plantuml.jar -DPLANTUML_LIMIT_SIZE=32768 -tpng -pipe > "$target_dir/$(basename "$file" .puml).png"
+done
 
 # generate diagrams with GraphViz dot
+find generated -type f -name "*.dot" | while read file; do
+  target_dir=$(dirname "$file")
+  mkdir -p "$target_dir"
+  dot "$file" -Tpng -o "$target_dir/$(basename "$file" .dot).png"
+done
 
 # copy results?
