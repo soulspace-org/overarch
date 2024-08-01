46ce6728a08f7fcc9caadf181c749768d70795d2
Ludger Solbach <soulman@soulspace.org>
2024-08-01 09:57:41 +0200

    updated comb tests

--- test/org/soulspace/overarch/adapter/template/comb_test.clj
+++ test/org/soulspace/overarch/adapter/template/comb_test.clj
@@ -5,18 +5,18 @@
 
 (deftest eval-test
   (testing "Comb templates evaluted with clojure.eval"
-    (is (= (cmb/eval (t/parse-template :comb "foo") {}) "foo"))
-    (is (= (cmb/eval (t/parse-template :comb "<%= 10 %>") {}) "10"))
-    (is (= (cmb/eval (t/parse-template :comb "<%= x %>") {:x "foo"}) "foo"))
-    (is (= (cmb/eval (t/parse-template :comb "<%=x%>") {:x "foo"}) "foo"))
-    (is (= (cmb/eval (t/parse-template :comb "<% (doseq [x xs] %>foo<%= x %> <% ) %>") {:xs [1 2 3]})
+    (is (= (cmb/eval (t/parse-template {:engine :comb} "foo") {}) "foo"))
+    (is (= (cmb/eval (t/parse-template {:engine :comb} "<%= 10 %>") {}) "10"))
+    (is (= (cmb/eval (t/parse-template {:engine :comb} "<%= x %>") {:x "foo"}) "foo"))
+    (is (= (cmb/eval (t/parse-template {:engine :comb} "<%=x%>") {:x "foo"}) "foo"))
+    (is (= (cmb/eval (t/parse-template {:engine :comb} "<% (doseq [x xs] %>foo<%= x %> <% ) %>") {:xs [1 2 3]})
            "foo1 foo2 foo3 "))))
 
 (declare x)
 (deftest fn-test
-  (is (= ((cmb/fn [x] (t/parse-template :comb "foo<%= x %>")) "bar")
+  (is (= ((cmb/fn [x] (t/parse-template {:engine :comb} "foo<%= x %>")) "bar")
          "foobar")))
 
 (deftest evalsci-test
   (testing "Comb templates evaluted with SCI"
-    (is (= (cmb/eval-sci (t/parse-template :combsci "foo") {}) "foo"))))
+    (is (= (cmb/eval-sci (t/parse-template {:engine :combsci} "foo") {}) "foo"))))
