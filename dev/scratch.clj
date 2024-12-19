a31d83a6abd81dcb49bbc46422242e1bddbf365e
Ludger Solbach <soulman@soulspace.org>
2024-12-19 09:33:17 +0100

    added snippet to scratch

---
+++ dev/scratch.clj
@@ -0,0 +1,16 @@
+
+;;
+;; Metadata Handling
+;; Source: Slack/Tim Pradley
+;;
+#_(defn assoc-meta [value k v]
+  "Adds `k` to value metadata if it can"
+  (cond (instance? IReference value) (do (alter-meta! value assoc k v) value)
+        (instance? IMeta value) (vary-meta value assoc k v)
+        :else value))
+
+#_(defn dissoc-meta [value k]
+  "Removes `k` from the metadata of value if present"
+  (cond (instance? IReference value) (do (alter-meta! value dissoc k) value)
+        (instance? IMeta value) (vary-meta value dissoc k)
+        :else value))
