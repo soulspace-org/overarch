18430b1178a96a2905ca73ed47146f09f5ee7437
Ludger Solbach <soulman@soulspace.org>
2025-04-19 00:46:42 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/tools.cli "1.1.230"]
-                 [com.cnuernber/charred "1.034"]
+                 [com.cnuernber/charred "1.037"]
                  [com.nextjournal/beholder "1.0.2"]
                  [expound/expound "0.9.0"]
-                 [org.babashka/sci "0.8.43"]
-                 [org.slf4j/slf4j-nop "2.0.16"]
+                 [org.babashka/sci "0.9.45"]
+                 [org.slf4j/slf4j-nop "2.0.17"]
                  [org.soulspace.clj/clj.java "0.9.1"]
                  [org.soulspace.clj/cmp.markdown "0.4.1"]]
 
